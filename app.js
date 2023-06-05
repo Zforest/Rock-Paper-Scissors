3404ec0b277c4746b1cf26d34cb306b4ff08460b
ZForest <30865263+Zforest@users.noreply.github.com>
2023-06-05 16:36:08 -0400

    Built function that randomly selects between r,p,s
    The function is intended for the computers selection

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello")
+let randomChoice = Math.floor(Math.random() * (4 - 1) + 1);
+
+function getComputerChoice() {
+    if (randomChoice === 1) {
+        console.log('Computer plays rock!')
+    } else if (randomChoice === 2) {
+        console.log('Computer plays paper!')
+    } else {
+        console.log('Computer plays scissors!')
+    }
+}
+console.log(getComputerChoice());
