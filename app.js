8610ee19e757eed0e659a943bbba50da7e828793
Zforest <30865263+Zforest@users.noreply.github.com>
2023-06-07 14:23:10 -0400

    Built function to run a single round of R,P,S
    The function will compare the player's entry to the computer's choice
    
    Then determines if the player wins or loses
    
    It will consider if the two match and declare a tie
    
    If the player types in anything other that rock, paper, scissors
    it will prompt re-entry

--- app.js
+++ app.js
@@ -9,33 +9,55 @@ Goals to complete Rock Paper Scissors
 
 Sub-problem to resolve now
 Create a single round of R,P,S
-  1. Obtain computer's choice
-  2. Obtain player's choice
-     a. Make the selection case insensitive
+  1. Obtain computer's choice (Complete)
+  2. Obtain player's choice (Complete)
+     a. Make the selection case insensitive (Complete)
   3. Compare the two choice's and declare a winner
-     a. Declare a tie if same choice is made
+     a. Declare a tie if same choice is made (Complete)
 */
 
 //Generates random number for the computer choice
-let randomChoice = Math.floor(Math.random() * (4 - 1) + 1);
+//Prompts player to make a choice and stores the input
+//Converts any input to lowercase for consistency
+let computerChoice = Math.floor(Math.random() * (4 - 1) + 1);
+let promptPlayerChoice = prompt("Please enter Rock, Paper, or Scissors");
+playerChoice = promptPlayerChoice.toLowerCase();
+
 
 //Instructs the computer to make a decision based on random number
+//Translates the random number to a string ('rock', 'paper', or 'scissors')
 function getComputerChoice() {
-    if (randomChoice === 1) {
-        console.log('Computer plays rock!')
-    } else if (randomChoice === 2) {
-        console.log('Computer plays paper!')
-    } else if (randomChoice === 3) {
-        console.log('Computer plays scissors!')
+    if (computerChoice === 1) {
+        computerChoice = 'rock';
+    } else if (computerChoice === 2) {
+        computerChoice = 'paper';
+    } else if (computerChoice === 3) {
+        computerChoice = 'scissors';
     }
 }
-//Displays computer choice in console
-getComputerChoice();
 
-//Prompts player to make a choice and stores the input
-let playerChoice = prompt("Please enter Rock, Paper, or Scissors");
-
-//One Round of R,P,S (Compares computer vs. players and determines result)
-function roundOne() {
-
+//One round of r,p,s
+//Compares player's choice to computer's choice and determines a winner and loser or a tie
+function roundOne (playerSelection, computerSelection) {
+    if (playerChoice === 'rock' && computerChoice === 'paper') {
+        return (`You played ${playerChoice} and the Computer plays ${computerChoice}. You Lose! Paper beats rock.`);
+    } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+        return (`You played ${playerChoice} and the Computer plays ${computerChoice}. You Win! Paper beats rock.`); 
+    } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+        return (`You played ${playerChoice} and the Computer plays ${computerChoice}. You Lose! Rock beats scissors.`);
+    } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+        return (`You played ${playerChoice} and the Computer plays ${computerChoice}. You Win! Rock beats scissors.`);
+    } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+        return (`You played ${playerChoice} and the Computer plays ${computerChoice}. You Lose! Scissors beats paper.`);
+    } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+        return (`You played ${playerChoice} and the Computer plays ${computerChoice}. You Win! Scissors beats paper.`);
+    } else if (playerChoice === computerChoice) {
+        return (`You played ${playerChoice} and the Computer plays ${computerChoice}. It\'s a tie! you both played ${playerChoice}.`);
+    } else {
+        return ('Incorrect entry. Please type rock, paper, or scissors');
+    }
 }
+
+const playerSelection = playerChoice;
+const computerSelection = getComputerChoice();
+console.log(roundOne(playerSelection, computerSelection));
