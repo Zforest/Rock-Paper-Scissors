345bbd694bf9a3093380e3e95648d869b33be1f4
Zforest <30865263+Zforest@users.noreply.github.com>
2023-06-21 15:03:48 -0400

    Replaced consolelogs with DIV that displays result

--- app.js
+++ app.js
@@ -2,16 +2,9 @@
 2. Tasks for UI
  a. Use buttons that the player selects instead of typing in moves
     i. 3 buttons. One for rock, paper, and scissors (Complete)
-    ii. Add event listener to each button that calls gameRound
-    with the correct playerSelection
-     a.query the id of each button (Complete)
-     b.create function that makes the playerSelection based on the button chosen
-     then triggers the gameRound with that playerSelection
-     c. apply function to each button via evenlistener (onclick)
-
-
+    ii. Add event listener to each button that calls gameRound (complete)
     iii. Add div that displays results and change all console.logs into
-    DOM methods
+    DOM methods (Complete)
     iv. Display a running score and announce the winner when 5 points
     is reached
     v. Rework old code as needed to make all above work
@@ -19,16 +12,37 @@
 4.  When done merge all changes to main branch (See instructions)
 */
 
+
 let playerScore = 0;
 let computerScore = 0;
 const rockButton = document.querySelector('#rockBtn');
 const paperButton = document.querySelector('#paperBtn');
 const scissorButton = document.querySelector('#scissorBtn');
+const roundResults = document.querySelector('#results');
+const roundWinner = document.createElement('div');
+const scoreUpdate = document.createElement('div');
+roundWinner.classList.add('winner');
+scoreUpdate.classList.add('score');
+
+
+//Anounces round winner, tie game, or game winning results 
+const scoreResults = () => {
+    if (computerScore > playerScore) {
+        scoreUpdate.textContent = `Computer is winning with a score of ${computerScore}. Your score is currently ${playerScore}.`;
+    } else if (playerScore > computerScore) {
+        scoreUpdate.textContent = `You are currently winning with a score of ${playerScore}. The Computer's score is currently ${computerScore}.`;
+    } else  if (computerScore === 5) {
+        scoreUpdate.textContent = 'Computer scores the final point and takes the game! Sorry, you lose. Try again!';
+    } else if (playerScore === 5) {
+        scoreUpdate.textContent = 'You score the final point and beat the computer! You win! Wanna play again?';
+    } else {
+        scoreUpdate.textContent = `The game is currently tied up at ${playerScore} to ${computerScore}.`;
+    }
+}
 
 //while (playerScore < 5 && computerScore < 5) {
 
-
-
+//Randomly selects a number between 1-3 and applies rock, paper, or scissors to it
 function getComputerChoice() {
     let randomNum = Math.floor(Math.random() * 3 + 1);
     switch (randomNum) {
@@ -38,62 +52,59 @@ function getComputerChoice() {
             return 'paper'
         case 3:
             return 'scissors'
-    
         }
 }
 const computerSelection = getComputerChoice(); 
 
+// Console logs who won the round or states if it was a tie.
 function gameRound (playerSelection, computerSelection) {
     if (
         (playerSelection === 'rock' && computerSelection === 'paper') ||
         (playerSelection === 'paper' && computerSelection ==='scissors') ||
         (playerSelection === 'scissors' && computerSelection === 'rock')
         ) {
-        console.log(`You played ${playerSelection} and the Computer plays ${computerSelection}. You Lose! ${computerSelection} beats ${playerSelection}.`);
-        return (computerScore++);
-
+            roundWinner.textContent = `You played ${playerSelection} and the Computer plays ${computerSelection}. You Lose! ${computerSelection} beats ${playerSelection}.`;
+            return computerScore++;
         } else if (
         (playerSelection === 'paper' && computerSelection === 'rock') ||
         (playerSelection === 'scissors' && computerSelection === 'paper') ||
         (playerSelection === 'rock' && computerSelection === 'scissors')
         ) {
-        console.log(`You played ${playerSelection} and the Computer plays ${computerSelection}. You Win! ${computerSelection} beats ${playerSelection}.`);
-        return (playerScore++);
+            roundWinner.textContent = `You played ${playerSelection} and the Computer plays ${computerSelection}. You Win! ${computerSelection} beats ${playerSelection}.`;
+            return playerScore++;
 
     } else if (playerSelection === computerSelection) {
-        console.log(`You played ${playerSelection} and the Computer plays ${computerSelection}. It\'s a tie! you both played ${playerSelection}.`);
+            roundWinner.textContent = `You played ${playerSelection} and the Computer plays ${computerSelection}. It\'s a tie! you both played ${playerSelection}.`;
             return;
     } else {
-        console.log('Incorrect entry. Please type rock, paper, or scissors');
+        roundWinner.textContent = 'Incorrect entry. Please type rock, paper, or scissors';
     }
 }
 
-
-function winnerResults () {
-    if (playerScore === 5) {
-        console.log('You Win! You beat the computer!');
-    } else if (computerScore === 5) {
-        console.log('You Lose! the computer Wins. Try again.');
-    }
-}
-
+//Event listeners that trigger on click of each button. The button also triggers the game round
 rockButton.addEventListener('click', function(e) {
     playerSelection = 'rock';
     gameRound(playerSelection, computerSelection);
-    console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
-    winnerResults();
+    scoreResults();
+    roundResults.appendChild(roundWinner);
+    roundResults.appendChild(scoreUpdate);
+
 });
 
 paperButton.addEventListener('click', function(e) {
     playerSelection = 'paper';
     gameRound(playerSelection, computerSelection);
-    console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
-    winnerResults();
+    scoreResults();
+    roundResults.appendChild(roundWinner);
+    roundResults.appendChild(scoreUpdate);
+
 });
 
 scissorButton.addEventListener('click', function(e) {
     playerSelection = 'scissors';
     gameRound(playerSelection, computerSelection);
-    console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
-    winnerResults();
+    scoreResults();
+    roundResults.appendChild(roundWinner);
+    roundResults.appendChild(scoreUpdate);
+
 });
